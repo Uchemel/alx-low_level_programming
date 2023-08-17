717f55c1d44f9fe546f4c3d45b3f8eb1ba296e16
UchechiEdward <edwarduchechi23@gmail.com>
2023-08-16 18:32:29 -0700

    Prints _putchar beginning with a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - prints _putchar
+ *
+ * Return: Always (0)
+ *
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
