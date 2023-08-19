763d60f3de4f7e09eacb96d488610b762d3708ce
UchechiEdward <edwarduchechi23@gmail.com>
2023-08-19 05:43:23 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -20,20 +20,19 @@ int main(void)
 		n /= 2;
 	}
 
-	for (i = 3; i <=sqrt(n); i = i + 2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
-			max =i;
-			n = n / i
+			max = i;
+			n = n / i;
 		}
 	}
 
 	if (n > 2)
 		max = n;
 
-	printf("%|d\n", max);
+	printf("%ld\n", max);
 
 	return (0);
-		
 }
