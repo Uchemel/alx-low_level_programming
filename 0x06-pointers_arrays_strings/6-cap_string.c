d8cad503220f4ac556b2c2aaab8f7b122685d5de
UchechiEdward <edwarduchechi23@gmail.com>
2023-08-24 16:58:53 -0700

    Capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,10 +18,10 @@ char *cap_string(char *str)
 					str[index - 1] == '\n' ||
 					str[index - 1] == ',' ||
 					str[index - 1] == ';' ||
-					str[index - 1] == '-' ||
+					str[index - 1] == '.' ||
 					str[index - 1] == '!' ||
 					str[index - 1] == '?' ||
-					str[index - 1] == "" ||
+					str[index - 1] == '"' ||
 					str[index - 1] == '(' ||
 					str[index - 1] == ')' ||
 					str[index - 1] == '{' ||
