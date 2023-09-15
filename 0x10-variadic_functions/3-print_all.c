1d204d808cf1242fff498be157e98d3343207cef
UchechiEdward <edwarduchechi23@gmail.com>
2023-09-15 16:21:09 -0700

    Write a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -25,6 +25,7 @@ void print_all(const char * const format, ...)
 					break;
 				case 'i':
 					printf("%s%d", sep, va_arg(list, int));
+					break;
 				case 'f':
 					printf("%s%f", sep, va_arg(list, double));
 					break;
