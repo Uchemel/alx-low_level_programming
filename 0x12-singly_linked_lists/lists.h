ab70d884a4554abcd86565ff96f7b63b29ab1ac4
UchechiEdward <edwarduchechi23@gmail.com>
2023-12-29 05:59:55 -0800

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,8 +4,8 @@
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * @len: the length of the string
+ * @next: it points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
@@ -15,7 +15,7 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_t;
+}list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
